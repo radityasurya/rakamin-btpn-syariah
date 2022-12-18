f2eea63cf2ec69b82fac8b169ad8f598e8bf5c26
radityasurya <radityasurya@hotmail.com>
2022-12-18 19:15:49 +0700

    feat: add vue router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>{{ judul }}</h1>
+    <input type="text" v-model="judul">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "About",
+  data() {
+    return {
+      judul: "Rakamin - BTPN Syariah",
+    }
+  }
+};
+</script>
